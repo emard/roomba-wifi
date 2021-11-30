64d05e4b0605521ea1e31e7fcab0a06baf2fe2b6
Emard <vordah@gmail.com>
2021-11-30 01:32:02 +0100

    fix disp script

--- scripts/roomba-disp.py
+++ scripts/roomba-disp.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
-import socket, sys
+import socket, sys, time
 disp = sys.argv[1][0:4].encode() # max 4 chars
 disp += b" " * (4-len(disp)) # pad length 4 with spaces
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(("192.168.48.116",23))
-s.send(bytearray([131, 164])+disp)
+s.send(bytearray([128, 131, 174]))
+time.sleep(0.05)
+s.send(bytearray([164])+disp)
 s.close()
