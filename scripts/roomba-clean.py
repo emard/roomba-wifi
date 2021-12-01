9d0488437a2e43096b68ea8edfb14a6cb92eb573
Emard <vordah@gmail.com>
2021-12-01 19:03:14 +0100

    delete old scripts, obsolete

--- scripts/roomba-clean.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-import socket
-# if roomba is already cleaning, this will stop it
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("192.168.48.116",23))
-s.send(bytearray([128, 135, 173])) # start, clean, stop
-s.close()
--- scripts/roomba-dock.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-import socket
-# roomba must be powered on and not cleaning
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("192.168.48.116",23))
-s.send(bytearray([128, 143, 173])) # start, seek dock command, stop
-s.close()
